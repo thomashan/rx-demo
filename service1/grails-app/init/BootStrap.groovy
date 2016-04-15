b6f0fb18f8c6eb037677c38133150856a38222cd
Thomas Han <thomas.han@nab.com.au>
2016-04-15 15:15:43 +1000

    added domain objects

--- service1/grails-app/init/BootStrap.groovy
+++ service1/grails-app/init/BootStrap.groovy
@@ -1,7 +1,20 @@
-class BootStrap {
+import au.com.nab.account.Account
+import au.com.nab.account.AccountsLoader
 
+class BootStrap {
     def init = { servletContext ->
+        Account.withTransaction {
+            new Account(name: "name",
+                accountNumber: "accountNumber",
+                balance: 0
+            ).save()
+        }
+//        Account.withTransaction {
+//            AccountsLoader.load(1000)
+//        }
     }
+
     def destroy = {
+        Account.findAll()*.delete()
     }
 }
