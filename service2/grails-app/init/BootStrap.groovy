c349045a1f0343a09328dca9dd8c95c51eda624a
Thomas Han <thomas.han@live.com>
2016-04-25 21:58:46 +1000

    use PostConstruct to listen on the request stream

--- service2/grails-app/init/BootStrap.groovy
+++ service2/grails-app/init/BootStrap.groovy
@@ -1,10 +1,5 @@
-import au.com.nab.deposit.DepositService
-
 class BootStrap {
-    DepositService depositService
-
     def init = { servletContext ->
-        depositService.listenToDepositMessages()
     }
     def destroy = {
     }
--- service2/grails-app/services/au/com/nab/deposit/DepositService.groovy
+++ service2/grails-app/services/au/com/nab/deposit/DepositService.groovy
@@ -8,11 +8,19 @@ import com.cj.kafka.rx.RxConsumer
 import groovy.json.JsonSlurper
 import kafka.serializer.StringDecoder
 
+import javax.annotation.PostConstruct
+
 import static rx.lang.scala.JavaConversions.toJavaObservable
 
 class DepositService {
     private static final String TOPIC = "service2"
+    static lazyInit = false
     RxConsumer rxConsumer = new RxConsumer("localhost:2181", "service2-group", false, false, null)
+
+    @PostConstruct
+    void initialise() {
+        listenToDepositMessages()
+    }
 
     def listenToDepositMessages() {
         JsonSlurper jsonSlurper = new JsonSlurper()
@@ -20,10 +28,9 @@ class DepositService {
         toJavaObservable(rxConsumer.getRecordStream(TOPIC, new StringDecoder(), new StringDecoder()))
                 .map({ Record record -> record.value().toString() })
                 .map({ message ->
-                    def json = jsonSlurper.parseText(message)
                     new Message(jsonSlurper.parseText(message))
                 })
-                // this is how you handle message types
+                // this is how you handle different message types
                 .filter({ message -> message.messageType == MessageType.REQUEST })
                 // This is how you handle multiple versions
                 .filter({ message -> message.version == "1" })
